cab2b0ce5aba902ff9d83ba63de8c7b1151deb55
Geogear <mustafaenesbilgic@hotmail.com>
2022-03-21 08:49:13 +0300

    Init.

---
+++ build.bat
@@ -0,0 +1,3 @@
+arm-none-eabi-gcc -mcpu=arm1176jzf-s -fpic -ffreestanding -c boot.S -o boot.o
+arm-none-eabi-gcc -fpic -ffreestanding -std=gnu99 -c kernel.c -o kernel.o -O2 -Wall -Wextra
+arm-none-eabi-gcc -T linker.ld -o primalos.elf -ffreestanding -O2 -nostdlib boot.o kernel.o
