62df5886f15d84539d23094dc4316c0691c7f4b4
Geogear <mustafaenesbilgic@hotmail.com>
2022-04-15 10:47:04 +0300

    Add os pt entry structure.

--- kernel.h
+++ kernel.h
@@ -6,14 +6,17 @@
 #define MB KB*KB
 #define PAGE_SIZE 4*KB
 
+#include "scp_registers.h"
+
+/* PAGE TABLE STRUCTURES */
 
 typedef struct{
-    uint32_t access_type:2; // [1:0] must be b01, it means page table base address. b00 is a translation fault
+    uint32_t access_type:2; // [1:0] must be b01, it means page table base address. b00 is a translation fault.
     uint32_t SBZ_1:1; // [2]
     uint32_t NS:1; // [3] TODO what's this?
     uint32_t SBZ_2:1; // [4]
-    uint32_t domain:4; // [8:5] defines the user or manager domain
-    uint32_t P:1; // [9] TODO look at this again
+    uint32_t domain:4; // [8:5] defines the user or manager domain.
+    uint32_t P:1; // [9] usage of this not supported by the processor.
     uint32_t coarse_pt_address:22; // [31:10]
 }first_lvl_entry;
 
@@ -25,10 +28,13 @@ typedef struct{
 }second_lvl_address;
 
 typedef struct{
-    uint32_t access_type:2; // [1:0] must be b10, for 4k pages
-    uint32_t B:1; // [2]
-    uint32_t C:1; // [3]
-    uint32_t AP_0:2; // [5:4]
+    uint32_t access_type:2; // [1:0] must be b10, for 4k pages.
+    uint32_t CB:2; // [3:2] C(acheable) and B(ufferable) bits,
+    // b00 -> strongly ordered, b01 -> Shared Device
+    // b10 -> Outer and Inner Write-Through, No Allocate on Write
+    // b11 -> Outer and Inner Write-Back, No Allocate on Write
+    // for more info, processor manual 6-15.
+    uint32_t AP_0:2; // [5:4] all AP_N bits must be zero.
     uint32_t AP_1:2; // [7:6]
     uint32_t AP_2:2; // [9:8]
     uint32_t AP_3:2; // [11:10]
@@ -39,5 +45,14 @@ typedef struct{
     uint32_t page_index:12; // [11:0]
     uint32_t page_base_address:20; // [31:12]
 }small_page_physical_address;
+
+typedef struct{
+    uint32_t allocated:1; // [0], if a process is using this page.
+    uint32_t dirty:1; // [1], 1 if a write operation is performed for this page.
+    uint32_t accessed:1; // [2], 1 if a read operation is performed for this page.
+    uint32_t available:1; // [3]
+    uint32_t pid:8; // [11:4] process using the page, 0 for the operating system.
+    uint32_t small_page_address:20; // [31:12]
+}os_pt_entry;
 
 #endif
