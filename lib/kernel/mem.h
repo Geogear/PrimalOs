efe23930d145d81723fdf42ed81cbf795336b4c2
Geogear <mustafaenesbilgic@hotmail.com>
2022-04-27 23:40:55 +0300

    Add page alloc/free.

--- lib/kernel/mem.h
+++ lib/kernel/mem.h
@@ -4,15 +4,17 @@
 #include <stdint.h>
 #include <kernel/atag.h>
 #include <kernel/kernel.h>
+#include <kernel/list.h>
 
 #define PAGE_SIZE 4096
 
 uint32_t get_num_p(void);
-os_pt_entry get_pt_entry(uint32_t index);
+uint32_t get_needed_page_count(uint32_t total_bytes);
+os_pt_entry* get_pt_entry(uint32_t index);
 void mem_init(atag_t* atags);
 
-/* Returns the index of allocated page, -1 on failure. */
-uint32_t alloc_page(uint32_t pid);
+/* Returns the address of the pg entry, NULL on failure. */
+os_pt_entry* alloc_page(uint32_t pid);
 
 /* Returns -1 on failure, 0 on success. */
 uint32_t free_page(uint32_t index);
--- src/kernel/mem.c
+++ src/kernel/mem.c
@@ -1,22 +1,34 @@
 #include <kernel/mem.h>
-#include <kernel/atag.h>
 #include <common/stdlib.h>
 #include <common/stdio.h>
-#include <stdint.h>
 #include <stddef.h>
 
-/* Defined in linker, end of the kernel image. */
+// Defined in linker, end of the kernel image.
 extern uint8_t __end;
 
+// All physical pages.
 static uint32_t num_pages;
 static os_pt_entry* all_pages;
 
+// First lvl page table for the vm.
 static const uint32_t first_lvl_num_entries = 1024;
 static first_lvl_entry* first_lvls;
 
+// Second lvl page tables for the vm.
 static const uint32_t second_lvl_num_entries = 256;
 static second_lvl_entry* second_lvls;
 
+// TODO Process table.
+static const uint32_t proc_t_num_entries = 256;
+
+// Generate a list type from the struct.
+DEFINE_LIST(os_pt_e);
+IMPLEMENT_LIST(os_pt_e);
+
+// Compiler generated list type.
+os_pt_e_list_t free_pages;
+
+// Kernel end with the page tables.
 static uint32_t kernel_end_addr;
 
 uint32_t get_num_p(void)
@@ -24,11 +36,20 @@ uint32_t get_num_p(void)
     return num_pages;
 }
 
-os_pt_entry get_pt_entry(uint32_t index)
+uint32_t get_needed_page_count(uint32_t total_bytes)
 {
-    if(index < num_pages)
-        return all_pages[index];
-    return all_pages[0];
+    if(total_bytes < PAGE_SIZE)
+        return 1;
+    if(total_bytes % PAGE_SIZE == 0)
+        return total_bytes / PAGE_SIZE;
+    return total_bytes / PAGE_SIZE + 1;
+}
+
+os_pt_entry* get_pt_entry(uint32_t index)
+{
+    if(num_pages <= index)
+        return NULL;
+    return &all_pages[index];
 }
 
 void mem_init(atag_t* atags) {
@@ -45,6 +66,7 @@ void mem_init(atag_t* atags) {
     // Set start for os pt.
     all_pages = (os_pt_entry*)&__end;
     bzero(all_pages, page_array_len_bytes);
+    INITIALIZE_LIST(free_pages);
 
     // Set start for first lvl table.
     first_lvls = (first_lvl_entry*)&all_pages[num_pages];
@@ -55,7 +77,6 @@ void mem_init(atag_t* atags) {
     bzero(second_lvls, first_lvl_num_entries*second_lvl_num_entries*sizeof(second_lvl_entry));
     
     kernel_end_addr = (uint32_t)&second_lvls[first_lvl_num_entries*second_lvl_num_entries];
-    //INITIALIZE_LIST(free_pages);
 
     // Iterate over all pages and mark them with the appropriate flags
     // Start with kernel pages
@@ -64,9 +85,12 @@ void mem_init(atag_t* atags) {
     puts("kernel pages:");
     log_uint(kernel_pages, 'h');
 
-    all_kernel_pages = kernel_end_addr / PAGE_SIZE;
+    all_kernel_pages = get_needed_page_count(kernel_end_addr);//kernel_end_addr / PAGE_SIZE;
     puts("all_kernel_pages:");
     log_uint(all_kernel_pages, 'h');
+
+    puts("size: ");
+    log_uint(sizeof(os_pt_entry), 'h');
 
     for (i = 0; i < all_kernel_pages; i++) {
         // Identity map the kernel pages
@@ -83,7 +107,7 @@ void mem_init(atag_t* atags) {
         // Only want the msb 22 bits.
         first_lvls[i].coarse_pt_address = tmp >> 10;
         // Leaving the domain as 0 for the kernel, meaning
-        // Domain 0 must be a manager domain.
+        // Domain not changed, so domain 0 must be a manager domain.
     }
 
     for(i = 0; i < second_lvl_num_entries * first_lvl_num_entries; ++i)
@@ -97,31 +121,48 @@ void mem_init(atag_t* atags) {
         // TODO cb, stays as strongly ordered for now.
     }
 
-
     // Map the rest of the pages as unallocated, and add them to the free list
-    /*for(; i < num_pages; i++){
-        all_pages_array[i].flags.allocated = 0;
-        append_page_list(&free_pages, &all_pages_array[i]);
-    }*/
+    for(i = all_kernel_pages; i < num_pages; ++i){
+        all_pages[i].allocated = 0;
+        append_os_pt_e_list(&free_pages, &all_pages[i]);
+    }
 
 }
 
-uint32_t alloc_page(uint32_t pid)
+os_pt_entry* alloc_page(uint32_t pid)
 {
+    os_pt_entry* entry = NULL;
+    // If no free pages left or pid is invalid.
+    if(size_os_pt_e_list(&free_pages) == 0 || proc_t_num_entries <= pid)
+        return entry;
 
+    // Get a page and set properties.
+    entry = pop_os_pt_e_list(&free_pages);
+    entry->allocated = 1;
+    entry->pid = pid;
+
+    // Zero out the page memory.
+    void* page_mem = (void*)(entry->small_page_index * PAGE_SIZE);
+    bzero(page_mem, PAGE_SIZE);
+
+    // IF you have the netry, you have the page memory.
+    return entry;
 }
 
 uint32_t free_page(uint32_t index)
 {
-    if(num_pages < index)
+    if(num_pages <= index)
         return -1;
     
-    /* Set all except the page index to zero. */
+    // Set all except the page index to zero.
     all_pages[index].allocated =
     all_pages[index].dirty =
     all_pages[index].accessed =
     all_pages[index].available =
     all_pages[index].pid = 0;
+
+    // Add it to the free list.
+    append_os_pt_e_list(&free_pages, &all_pages[index]);
 
     return 0;
 }
