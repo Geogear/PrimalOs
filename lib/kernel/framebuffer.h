a090792de8d12be7028165186a492d57af13906d
Geogear <mustafaenesbilgic@hotmail.com>
2022-04-29 10:33:16 +0300

    Add fb data structure definition.

---
+++ lib/kernel/framebuffer.h
@@ -0,0 +1,25 @@
+
+#ifndef FRAMEBUFFER_H
+#define FRAMEBUFFER_H
+#include <stdint.h>
+
+#define COLORDEPTH 24
+#define BYTES_PER_PIXEL COLORDEPTH/8
+
+typedef struct framebuffer_info {
+    uint32_t width;
+    uint32_t height;
+    uint32_t pitch;
+    void * buf;
+    uint32_t buf_size;
+    uint32_t chars_width;
+    uint32_t chars_height;
+    uint32_t chars_x;
+    uint32_t chars_y;
+} framebuffer_info_t;
+
+extern framebuffer_info_t fbinfo;
+
+int framebuffer_init(void);
+
+#endif
