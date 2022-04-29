0a251d54b6c2c29b393feb1e449eaae3b956a12d
Geogear <mustafaenesbilgic@hotmail.com>
2022-04-29 10:33:48 +0300

    Add gpu api and pixel definition.

---
+++ lib/kernel/gpu.h
@@ -0,0 +1,22 @@
+#ifndef GPU_H
+#define GPU_H
+
+#include <kernel/peripheral.h>
+#include <stdint.h>
+
+#define CHAR_WIDTH 8
+#define CHAR_HEIGHT 8
+
+typedef struct pixel {
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+} pixel_t;
+
+void gpu_init(void);
+
+void write_pixel(uint32_t x, uint32_t y, const pixel_t * pixel);
+
+void gpu_putc(char c);
+
+#endif
